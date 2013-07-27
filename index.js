2531f7c691af011917d83b8dd1caf74d033ccc44
Tim Cameron Ryan <id@timryan.org>
2013-07-27 13:20:20 -0400

    Handshaking

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var choices = require('choices')
 
 
 if (process.argv.length < 3) {
-  console.error('Usage: ss [-i] [filename]');
+  console.error('ScriptStick CLI\nUsage: ss [-i] [filename]');
   process.exit(1);
 }
 
@@ -43,6 +43,13 @@ detectDevice(function (modem) {
   handshake(modem, function (serial) {
     if (process.argv[2] == '-i') {
       console.log('[connected]'.grey);
+
+      // console.log('Beat.');
+      // setTimeout(function () {
+      //   serial.send('B', '', function () {
+      //     console.log('BREAK');
+      //   })
+      // }, 5000);
     } else {
       compile(process.argv[2], function (luacode) {
         upload(serial, luacode);
@@ -79,25 +86,44 @@ function handshake (modem, next) {
 
   var serial = jssc.listen(modem);
   serial.stderr.pipe(process.stderr);
+
   serial.on('close', function (code) {
     console.log('jssc exited with code', code);
     process.exit(1);
   })
+  serial.on('cts', function (cts) {
+    if (!cts) {
+      process.exit(1);
+    }
+  })
 
   // Wait for initial "!\n"
-  serial.stdout.once('data', function onhandshake (data) {
-    serial.stdout.pipe(process.stdout);
-    serial.stdin.write("!\n", function () {
-      console.log('done.');
-      next(serial);
-    });
+  var shake = '';
+  serial.on('data', function onhandshake (data) {
+    shake += String(data);
+    if (shake == '!\n') {
+      serial.removeListener('data', onhandshake);
+      serial.on('data', function (data) {
+        process.stdout.write(data);
+      });
+      serial.on('error', function (err) {
+        console.error(err);
+      });
+      serial.write("!\n", function () {
+        console.log('done.');
+        next(serial);
+      });
+    } else if (shake.length > 2) {
+      console.error('ERROR'.red, 'Please restart device.');
+      process.exit(1);
+    }
   })
 }
 
 function upload (serial, luacode) {
   var sizebuf = new Buffer(4);
   sizebuf.writeInt32LE(luacode.length, 0);
-  serial.stdin.write(Buffer.concat([sizebuf, luacode]), function () {
+  serial.write(Buffer.concat([sizebuf, luacode]), function () {
     console.log(String('[it is written]').grey);
   });
 }
