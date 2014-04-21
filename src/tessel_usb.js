61eaf79e498afa6192e857df10b09142e396a29a
Kevin Mehall <km@kevinmehall.net>
2014-04-21 13:38:44 -0400

    Fix debug-stack for API changes from km-event-loop.
    When lua is not running, it returns that status with the control reply
    instead of sending a bulk message later.

--- src/tessel_usb.js
+++ src/tessel_usb.js
@@ -178,21 +178,28 @@ Tessel.prototype.stop = function stop(next) {
 }
 
 Tessel.prototype.debugstack = function stop(next) {
-	this.usb.controlTransfer(VENDOR_REQ_OUT, REQ_STACK_TRACE, 0, 0, new Buffer(0), function(err) {
-		if (err) {
-			this.removeListener('command', oncommand);
-			next(err);
-		}
+	this.usb.controlTransfer(VENDOR_REQ_IN, REQ_STACK_TRACE, 0, 0, 16, function(err, buf) {
+		if (err) return callNext(err);
+
+		var r = buf.readInt8(0);
+		if (r != 0) return callNext(null, false);
+		// otherwise, wait for the message
 	});
+
 
 	function oncommand(command, msg) {
 		if (command == 'k') {
-			this.removeListener('command', oncommand);
-			next(null, msg.toString());
+			callNext(null, msg.toString());
 		}
 	}
 
 	this.on('command', oncommand)
+
+	var self = this;
+	function callNext(err, data) {
+		self.removeListener('command', oncommand);
+		next(err, data);
+	}
 }
 
 Tessel.prototype.wifiIP = function (next) {
