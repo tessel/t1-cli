1b6044101c097f8ddcad28cccf26748095953e25
jialiya <jialiya.huang0@gmail.com>
2014-07-01 12:55:20 -0700

    fixing test

--- test/test_symlinks.js
+++ test/test_symlinks.js
@@ -4,7 +4,7 @@ var tar = require('tar');
 console.log('1..1')
 tessel.bundleFiles('index.js', [], {
 	'sym/sym2/index.js': __dirname + '/sym/sym2/index.js'
-}, function (err, bundle) {
+}, {}, function (err, bundle) {
 	tar.Parse()
 		.on('entry', function (data) {
 			if (data.path == 'app/sym/sym2/index.js') {
